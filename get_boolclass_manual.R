405bb6e2e6ac0f3a530c3c6499c7d03ba8407667
Andrew Yates <andrewyates.name@gmail.com>
2013-01-27 22:48:55 -0500

    updates

--- get_boolclass_manual.R
+++ get_boolclass_manual.R
@@ -1,20 +1,9 @@
-#CLASSES_I = {
-#  'UNL': 0,
-#  'HIH': 1,
-#  'PC': 2,
-#  'LIL': 3,
-#  'HIL': 4,
-#  'NC': 5,
-#  'LIH': 6,
-#}
-
 #install.packages("modeest")
 library("RColorBrewer")
 library(modeest)
 library("gplots")
 
 setwd("/nfs/01/osu6683")
-
 
 source("density_merge_bicliques/bicliques_t.45_o.55_f.16_areamerge.R")
 BC0<-DCOR[bcbest.0.row,bcbest.0.col]
@@ -41,66 +30,27 @@ BC0 <- read.table("bc0.dcor.tab", header=TRUE, sep=" ", row.names=1);
 BCBig.cls <- read.table("bcBig.cls.tab", header=TRUE, sep=" ", row.names=1);
 BCBig <- read.table("bcbig.dcor.tab", header=TRUE, sep=" ", row.names=1);
 # ---------------
-
-# calc mode
-# mlv(as.matrix(BC0.cls))
-
-int.hist <- function(x,ylab="Frequency",...) {
-  barplot(table(factor(x,levels=min(x):max(x))),space=0,xaxt="n",ylab=ylab,...);axis(1)
-}
-
-
-# COMPUTE DISTANCE MATRIX
-# ====================
-# define glyph pairwise distance
-r1<-c(0,1,1,1,1,1,1,4)
-r2<-c(1,0,1,2,2,2,2,4)
-r3<-c(1,1,0,1,2,3,2,4)
-r4<-c(1,2,1,0,2,2,2,4)
-r5<-c(1,2,2,2,0,1,2,4)
-r6<-c(1,2,3,2,1,0,1,4)
-r7<-c(1,2,2,2,2,1,0,4)
-r8<-c(4,4,4,4,4,4,4,0)
-dist.glyph <- matrix(c(r1,r2,r3,r4,r5,r6,r7,r8), 8,8)
-
-
-# Compute distance between two vectors of glyphs
-glyph.f <- function(A,B) {
-  # glyphs are enumerated from zero already
-  sqrt(sum(dist.glyph[as.numeric(A*8+(B+1))]**2))
-}
-
-# Compute distance matrix (this is rather slow, but faster than other options
-gen.glyph.dist.m <- function(BC) {
-  D <- matrix(0,nrow(BC),nrow(BC))
-  for (i in 1:nrow(BC)) {
-    for (j in (i+1):nrow(BC)) {
-      if (j <= nrow(BC)) {
-        D[j,i] <- glyph.f(BC[i,], BC[j,])
-      }
-    }
-  }
-  as.dist(D)
-}
-
 # Re-enumerate glyph symbols
 #   1,2,3 to 0,1,2
 #   0 to 3
-
 renumerate <- function(BC) {
   BC[BC==0] <- -1
   BC[BC>=1 & BC<=3] <- BC[BC>=1 & BC<=3] -1
   BC[BC==-1] <- 3
   BC
 }
+BC0.cls <- renumerate(BC0.cls)
+BCBig.cls <- renumerate(BCBig.cls)
 
-# 
+
+# calc mode
+# mlv(as.matrix(BC0.cls))
 
 # Get indices from hierarchical clustering
 D.DCOR.r <- as.dist(1-cor(t(BC0), method="pearson")) + dist(BC0)
 D.DCOR.c <- as.dist(1-cor(BC0, method="pearson")) + dist(t(BC0))
-D.cls.r <- dist(renumerate(BC0.cls))
-D.cls.c <- dist(t(renumerate(BC0.cls)))
+D.cls.r <- dist(BC0.cls)
+D.cls.c <- dist(t(BC0.cls))
 Rowv <- rowMeans(BC0, na.rm = TRUE)
 Colv <- colMeans(BC0, na.rm = TRUE)
 
@@ -217,3 +167,40 @@ report.cls(BCBig.cls,"Big")
     key=TRUE, symkey=FALSE, trace="none",
     Rowv=as.dendrogram(Rhclust), Colv=as.dendrogram(Chclust)
     );
+
+
+
+
+
+# COMPUTE DISTANCE MATRIX
+# ====================
+# define glyph pairwise distance (from original UNL==0 enumeration)
+r1<-c(0,1,1,1,1,1,1,4)
+r2<-c(1,0,1,2,2,2,2,4)
+r3<-c(1,1,0,1,2,3,2,4)
+r4<-c(1,2,1,0,2,2,2,4)
+r5<-c(1,2,2,2,0,1,2,4)
+r6<-c(1,2,3,2,1,0,1,4)
+r7<-c(1,2,2,2,2,1,0,4)
+r8<-c(4,4,4,4,4,4,4,0)
+dist.glyph <- matrix(c(r1,r2,r3,r4,r5,r6,r7,r8), 8,8)
+
+
+# Compute distance between two vectors of glyphs
+glyph.f <- function(A,B) {
+  # glyphs are enumerated from zero already
+  sqrt(sum(dist.glyph[as.numeric(A*8+(B+1))]**2))
+}
+
+# Compute distance matrix (this is rather slow, but faster than other options
+gen.glyph.dist.m <- function(BC) {
+  D <- matrix(0,nrow(BC),nrow(BC))
+  for (i in 1:nrow(BC)) {
+    for (j in (i+1):nrow(BC)) {
+      if (j <= nrow(BC)) {
+        D[j,i] <- glyph.f(BC[i,], BC[j,])
+      }
+    }
+  }
+  as.dist(D)
+}
